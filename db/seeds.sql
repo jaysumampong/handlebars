7816d05d70f70b22b9c2fb2313cc8b9820943846
Jay Sumampong <jaysumampong@gmail.com>
2019-10-23 15:22:42 -0600

    added server

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+
+INSERT INTO burgers_db(burger_name)
+VALUES
+    ('McDouble'),
+    ('Heart-Attack Burger'),
+    ('Veggie Burger'),
+    ('Double Double Animal-Style');
--- server.js
+++ server.js
@@ -1,4 +1,21 @@
 let express = require("express");
 let mysql = require("mysql");
-let handlebars = require("express-handlebars");
+let exphbs = require("express-handlebars");
+let methodOverride = require("method-override");
+let bodyParser = require("body-parser");
 
+let app = express();
+app.use(bodyParser.urlencoded({
+    exended: false
+}))
+app.use(methodOverride("_method"));
+app.engine("handlebars", exphbs({defaultLayout:"main"}));
+app.set("view engine", "handlebars");
+
+let PORT = process.env.PORT || 3000;
+app.listen(PORT, function() {
+    console.log("now listening at" + PORT);
+});
+
+let routes = require("./controllers/burgers_controllers.js");
+app.use(routes);
